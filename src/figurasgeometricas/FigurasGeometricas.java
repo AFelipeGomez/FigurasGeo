a5fbcbdf49664ee2a4b3a925b6a12edc0ab32f29
AFelipeGomez <55608159+AFelipeGomez@users.noreply.github.com>
2020-02-27 13:04:37 -0500

    Clase FigurasGeometricas con primeras variables y metodos get

--- src/figurasgeometricas/FigurasGeometricas.java
+++ src/figurasgeometricas/FigurasGeometricas.java
@@ -11,11 +11,44 @@ package figurasgeometricas;
  */
 public class FigurasGeometricas {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   private int coordenada1;
+    private int coordenada2;
+    private int coordenada3;
+    private int perimetro;
+    private int area;
+    private int color;
+
+    public FigurasGeometricas(int coordenada1, int coordenada2, int coordenada3, int color) {
+
+        this.color = color;
+        this.coordenada1 = coordenada1;
+        this.coordenada2 = coordenada2;
+        this.coordenada3 = coordenada3;
+        this.color = color;
+
     }
-    
+
+    public int getCoordenada1() {
+        return coordenada1;
+    }
+
+    public int getCoordenada2() {
+        return coordenada2;
+    }
+
+    public int getCoordenada3() {
+        return coordenada3;
+    }
+
+    public int getPerimetro() {
+        return perimetro;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public int getColor() {
+        return color;
+    }
 }
